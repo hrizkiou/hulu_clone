0c0a247d28ba13270c6d47b3906e1d31334a68ac
Hamza Riz <hamzarizkioui@hotmail.com>
2021-05-09 11:13:26 +0000

    working on grid-display

--- components/thumbnail.js
+++ components/thumbnail.js
@@ -3,7 +3,7 @@ export default function Thumbnail({ result }) {
     console.log(result);
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return(
-        <div className='m-5'>
+        <div className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
             <Image
                 layout='responsive'
                 src={
@@ -14,8 +14,8 @@ export default function Thumbnail({ result }) {
             />
             <div className='p-2'>
                 <p className='truncate max-w-md'>{result.overview}</p>
-                <h2>{result.title || result.original_name}</h2>
-                <p>
+                <h2 className='mt-1 text-2x1 text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{result.title || result.original_name}</h2>
+                <p className='flex items-center opacity-0 group-hover:opacity-100'>
                     {/*{result.media_type && `${result.media_type} ` {" "}}*/}
                 </p>
             </div>
